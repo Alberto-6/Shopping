9c8dcb557ba5c9ee4b6ba9bd7cc0b9da3d181275
Alberto-6 <66850211+Alberto-6@users.noreply.github.com>
2022-06-21 21:13:33 -0400

    Creacion de la plantilla
    Creacion de la plantilla para el proyecto

---
+++ Shopping/Data/Entities/Country.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopping.Data.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Pais")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener {1} caracteres")]
+        [Required(ErrorMessage = "El Campo [0] es obligatorio")]
+        public string Name { get; set; }
+    }
+}
