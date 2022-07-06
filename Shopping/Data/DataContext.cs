cdfd0b228741fd1f4333acf410d1b32b4a6ca934
Alberto-6 <66850211+Alberto-6@users.noreply.github.com>
2022-07-05 21:12:22 -0400

    Se creo el controller Countries con sus vistas, agregando el crud con las funciones delete, edit, create.

--- Shopping/Data/DataContext.cs
+++ Shopping/Data/DataContext.cs
@@ -15,5 +15,11 @@ namespace Shopping.Data
         }
 
         public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+        }
     }
 }
