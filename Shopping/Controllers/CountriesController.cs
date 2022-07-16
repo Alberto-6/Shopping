c9e4eb3a883e4c3a7f1928f1453886210998aa9f
Alberto-6 <66850211+Alberto-6@users.noreply.github.com>
2022-07-15 21:42:17 -0400

    Finish countries crud

--- Shopping/Controllers/CountriesController.cs
+++ Shopping/Controllers/CountriesController.cs
@@ -58,15 +58,35 @@ namespace Shopping.Controllers
         {
             if (ModelState.IsValid)
             {
-                _context.Add(country);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                try
+                {
+                    _context.Add(country);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateException dbUpdateException)
+                {
+                    if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                    {
+                        ModelState.AddModelError(string.Empty, "Ya existe un país con el mismo nombre.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    ModelState.AddModelError(string.Empty, exception.Message);
+                }
             }
             return View(country);
-        }
+      
 
-        // GET: Countries/Edit/5
-        public async Task<IActionResult> Edit(int? id)
+    }
+
+    // GET: Countries/Edit/5
+    public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
             {
